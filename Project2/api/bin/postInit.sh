8db3032dfe852e1f44f361903541eb894c1227ac
Spencer5515 <spencer.demera@csu.fullerton.edu>
2021-10-23 21:56:01 -0700

    Delete postInit.sh

--- Project2/api/bin/postInit.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# Initialization bash file for posts microservice API
-
-sqlite-utils insert ./data/posts.db posts --csv ./data/posts.csv --detect-types --pk=author_username
-sqlite-utils create-index ./data/posts.db posts message human_timestamp timestamp origin_URL --unique
